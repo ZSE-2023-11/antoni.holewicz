45a1906e86db2afef6ec66d72b759c9f660bc839
AntoniHolewicz <antoni.holewicz@icloud.com>
2023-11-27 09:49:23 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Logowanie</title>
+</head>
+<body>
+
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if ($_POST["username"] === "admin" && $_POST["password"] === "test") {
@@ -7,3 +16,16 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 }
 ?>
+
+<form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+    <label for="username">Username:</label>
+    <input type="text" id="username" name="username" required><br>
+
+    <label for="password">Password:</label>
+    <input type="password" id="password" name="password" required><br>
+
+    <input type="submit" value="Submit">
+</form>
+
+</body>
+</html>
