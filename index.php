1a2d72682067b5953b36968fcd486ccb2b946ec1
AntoniHolewicz <antoni.holewicz@icloud.com>
2023-11-27 09:11:39 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,26 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="author" content="Seweryn Mocny">
-    <title>Zadanie 2</title>
-    <link rel="stylesheet" href="style1.css">
-</head>
-<body>
-    <div class="container">
-        <div class="title-box">
-            <h1>Logowanie</h1>
-        </div>
-        <div class="input-box">
-            <form method="post">
-                <input name="username" id="username" type="text" required>
-                <input name="password" id="password" type="password" required>
-                <button id="button-login">Zaloguj</button>
-            </form>
-        </div>
-    </div>
-
     <?php
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $usernameInput = htmlspecialchars($_POST["username"]);
@@ -35,5 +12,3 @@
         }
     }
 ?>
-</body>
-</html>
